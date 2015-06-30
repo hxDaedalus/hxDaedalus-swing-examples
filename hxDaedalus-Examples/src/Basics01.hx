f336515b4ee03c21489f6cd534981ab6a701f6e9
Justin Mills <jlm@justinfront.net>
2015-06-30 17:58:47 +0100

    modified to work with 'wings' and 'hxPixels', also added compileAll.hxml

--- hxDaedalus-Examples/src/Basics01.hx
+++ hxDaedalus-Examples/src/Basics01.hx
@@ -5,7 +5,7 @@ import hxDaedalus.data.Object;
 import hxDaedalus.data.Vertex;
 import hxDaedalus.factories.RectMesh;
 import hxDaedalus.view.SimpleView;
-import hxDaedalus.swing.BasicSwing;
+import wings.javaSwing.BasicSwing;
 import java.awt.Graphics2D;
 import haxe.Timer;
 
@@ -13,49 +13,49 @@ class Basics01 extends BasicSwing {
     var mesh: 	Mesh;
     var view: 	SimpleView;
     var object: Object;
-	
+
     public static function main(): Void {
-		new Basics01();	
+		new Basics01();
 	}
-	
+
     public function new()
     {
 		super();
-		
+
         // build a rectangular 2 polygons mesh of 600x400
         mesh = RectMesh.buildRectangle(600, 400);
-		
+
         // create a viewport
         view = new SimpleView(this);
         surface.paintFunction = paintFunction;
-	
+
         // SINGLE VERTEX INSERTION / DELETION
         // insert a vertex in mesh at coordinates (550, 50)
         var vertex : Vertex = mesh.insertVertex(550, 50);
         // if you want to delete that vertex :
         //_mesh.deleteVertex(vertex);
-        
-        
+
+
         // SINGLE CONSTRAINT SEGMENT INSERTION / DELETION
         // insert a segment in mesh with end points (70, 300) and (530, 320)
         var segment : ConstraintSegment = mesh.insertConstraintSegment(70, 300, 530, 320);
         // if you want to delete that edge
         //_mesh.deleteConstraintSegment(segment);
-        
-        
+
+
         // CONSTRAINT SHAPE INSERTION / DELETION
         // insert a shape in mesh (a crossed square)
-        var shape = mesh.insertConstraintShape( [   
+        var shape = mesh.insertConstraintShape( [
                         50., 50., 100., 50.,        /* 1st segment with end points (50, 50) and (100, 50)       */
                         100., 50., 100., 100.,      /* 2nd segment with end points (100, 50) and (100, 100)     */
                         100., 100., 50., 100.,      /* 3rd segment with end points (100, 100) and (50, 100)     */
                         50., 100., 50., 50.,        /* 4rd segment with end points (50, 100) and (50, 50)       */
                         20., 50., 130., 100.        /* 5rd segment with end points (20, 50) and (130, 100)      */
-                                                ] );      
+                                                ] );
         // if you want to delete that shape
         //_mesh.deleteConstraintShape(shape);
-        
-        
+
+
         // OBJECT INSERTION / TRANSFORMATION / DELETION
         // insert an object in mesh (a cross)
         var objectCoords : Array<Float> = new Array<Float>();
@@ -66,7 +66,7 @@ class Basics01 extends BasicSwing {
                                 -30., -30.,  30., 30.,
                                  30., -30., -30., 30.
                                 ];
-        mesh.insertObject( object );  // insert after coordinates are setted  
+        mesh.insertObject( object );  // insert after coordinates are setted
         // you can transform objects with x, y, rotation, scaleX, scaleY, pivotX, pivotY
         object.x = 400;
         object.y = 200;
@@ -74,24 +74,24 @@ class Basics01 extends BasicSwing {
         object.scaleY = 2;
         // if you want to delete that object
         //_mesh.deleteObject(_object);
-	
+
         // animate
         var timer = new Timer( Math.floor( 1000/60 ) );
         timer.run = renderTimer;
-	}	
-	
+	}
+
     function renderTimer():Void {
         // objects can be transformed at any time
         object.rotation += 0.05;
-    
-        mesh.updateObjects();  // don't forget to update  
+
+        mesh.updateObjects();  // don't forget to update
         // render mesh
         surface.repaint();
     }
-    
+
     function paintFunction( g: Graphics2D ):Void {
         view.refreshGraphics2D( g );
         view.drawMesh( mesh, true );
     }
 
-}	
+}
